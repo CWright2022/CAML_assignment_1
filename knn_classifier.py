b8d7f36ca82c2e9e866dd69e29c107c9f3f353f2
zriback <zacharythecdr@gmail.com>
2025-09-04 15:32:46 -0400

    I think it works but is very slow. Need to convert arrays at the end to numpy before doing the distance calculations

--- knn_classifier.py
+++ knn_classifier.py
@@ -101,7 +101,7 @@ def calculate_sms_tf(vocabulary: list[str], sms: str) -> list[float] | None:
     return term_frequency_list
 
 
-def calculate_sms_idf(vocabulary: list[str], sms: str, tokenized_sms_list: list[list[str]]) -> list[float] | None:
+def calculate_sms_idf(vocabulary: list[str], sms: str, tokenized_sms_list: list[list[str]], testing_sample=False) -> list[float] | None:
     """
     Calculate the inverse document frequency for a given SMS message
     For each token in the vocabulary: log( (total # of SMS messages) / (# of messages this word appears in) ) for each word in this SMS
@@ -111,6 +111,7 @@ def calculate_sms_idf(vocabulary: list[str], sms: str, tokenized_sms_list: list[
         vocabulary (list[str]): set of all words present in the samples
         sms (str): the given SMS message
         sms_list (list[str]): list of all SMS messages
+        testing_sample (bool=False): if this sample is not in the training set, must add one to the denominator
     Returns:
         list[float]: inverse document frequency calculations given this message for each word in our vocabulary and this set of SMS messages
         Will return None if the given SMS has no tokens. For example, with certain cleaning methods an SMS will have no tokens
@@ -124,7 +125,7 @@ def calculate_sms_idf(vocabulary: list[str], sms: str, tokenized_sms_list: list[
     # Calculate idf for this sms
     for token in vocabulary:
         if token in tokenized_sms:
-            token_appearance_count = 0
+            token_appearance_count = 1 if testing_sample else 0
             for tokens in tokenized_sms_list:
                 if token in tokens:
                     token_appearance_count += 1
@@ -132,6 +133,28 @@ def calculate_sms_idf(vocabulary: list[str], sms: str, tokenized_sms_list: list[
         else:
             inverse_document_frequency_list.append(0)
     return inverse_document_frequency_list
+
+
+def calculate_n_dimensional_distance(p1: list[float], p2: list[float]) -> float:
+    """
+    Calculates the distance between two points in n-dimensional space
+
+    Args:
+        p1 (list[float]): the first point
+        p2 (list[float]): the second point
+    Returns:
+        float: The distance between the points
+    """
+    if len(p1) != len(p2):
+        raise ValueError('The two provided points do not have the same dimensionality')
+    squares_sum = 0
+    for i in range(len(p1)):
+        val1 = p1[i]
+        val2 = p2[i]
+        difference_sqaure = math.pow(val1 - val2, 2)
+        squares_sum += difference_sqaure
+    result = math.sqrt(squares_sum)
+    return result
 
 
 def main():
@@ -171,9 +194,11 @@ def main():
         tokenized_sms_list.append(tokenize_sms(sms))
 
     # Calculate term frequencies and inverse document frequencies for each SMS in the training set
+    # Also calculate the TF-IDF by multiplying both values together
     sms_term_frequencies = {}
     sms_inverse_document_frequencies = {}
-    for sms in sms_samples.keys():
+    sms_tf_idf = {}
+    for sms in list(sms_samples.keys()):
         sms_tf = calculate_sms_tf(vocabulary, sms)
         if sms_tf is None:
             continue
@@ -184,7 +209,87 @@ def main():
             continue
         sms_inverse_document_frequencies[sms] = sms_idf
 
-    print(sms_inverse_document_frequencies)
+        sms_tf_idf[sms] = [tf * idf for tf, idf in zip(sms_tf, sms_idf)]
+    
+    # Testing
+    ground_truth = {}
+    for sms in ham_list_test:
+        ground_truth[sms] = 0
+    for sms in spam_list_test:
+        ground_truth[sms] = 1
+    
+    results = {}
+    for sms in list(ground_truth.keys()):
+        tf = calculate_sms_tf(vocabulary, sms)
+        idf = calculate_sms_idf(vocabulary, sms, tokenized_sms_list, True)
+        tf_idf = list(tf_val * idf_val for tf_val, idf_val in zip(tf, idf))
+
+        # Calculate the distance between this vector and the vector for every point in the training set
+        distances = {}
+        for training_sms, training_tf_idf in sms_tf_idf.items():
+            distances[training_sms] = calculate_n_dimensional_distance(training_tf_idf, tf_idf)
+        
+        # Sort dictionary by value and select the K lowest values
+        # If spam/ham has the majority in the K lowest values, then that is our prediction
+        distances = dict(sorted(distances.items(), key=lambda item: item[1]))
+        ham_count = 0
+        spam_count = 0
+        for sms in list(distances.keys())[:K]:
+            if sms_samples[sms] == 0:
+                ham_count += 1
+            else:
+                spam_count += 1
+            results[sms] = 0 if ham_count > spam_count else 1
+            print(f'{sms[:150]}...')
+            print('ham' if ham_count > spam_count else 'spam')
+            print()
+        
+    # Results and performance metrics
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    for sms, prediction in results.items():
+        print(f'{sms[:150]:<160}', end='')
+        if prediction:
+            print(f'{COLOR_RED}SPAM{COLOR_DEFAULT}\t', end='')
+            if ground_truth[sms] == prediction:  # True positive
+                print(f'{COLOR_GREEN}TP{COLOR_DEFAULT}')
+                tp += 1
+            else:  # False positive
+                print(f'{COLOR_RED}FP{COLOR_DEFAULT}')
+                fp += 1
+        else:
+            print(f'{COLOR_GREEN}HAM{COLOR_DEFAULT}\t', end='')
+            if ground_truth[sms] == prediction:  # True negative
+                print(f'{COLOR_GREEN}TN{COLOR_DEFAULT}')
+                tn += 1
+            else:  # False negative
+                print(f'{COLOR_RED}FN{COLOR_DEFAULT}')
+                fn += 1
+
+    print()
+    print('Totals:')
+    print(f'True positives: {tp}')
+    print(f'False positives: {fp}')
+    print(f'True negatives: {tn}')
+    print(f'False negatives: {fn}')
+
+    accuracy = (tp + tn) / (tp + fp + tn + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1_score = (2 * precision * recall) / (precision + recall)
+
+    print()
+    print('Metrics:')
+    print(f'Accuracy: {accuracy*100:.2f}%')
+    print(f'Precision: {precision*100:.2f}%')
+    print(f'Recall: {recall*100:.2f}%')
+    print(f'F1-score: {f1_score:.2f}')
+
+            
+
+
 
 
 if __name__ == '__main__':
