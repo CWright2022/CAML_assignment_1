27ced8bf230445306dc9a7c91448ae3bc3fabf0f
zriback <zacharythecdr@gmail.com>
2025-09-04 20:04:13 -0400

    the knn runs in a reasonable amount of time and I think it works

--- knn_classifier.py
+++ knn_classifier.py
@@ -5,6 +5,7 @@ import os
 import math
 from random import shuffle
 import numpy as np
+import numpy.typing as npt
 
 # Color codes
 COLOR_DEFAULT = '\x1b[39m'
@@ -77,7 +78,7 @@ def get_tokens(sms_list: list[str]) -> list[str]:
     return tokens_list
 
 
-def calculate_sms_tf(vocabulary: list[str], sms: str) -> list[float] | None:
+def calculate_sms_tf(vocabulary: list[str], sms: str) -> npt.NDArray[np.float64] | None:
     """
     Calculates the term frequency for a SMS message
     For each unique token in this message, calculates what proportion of the tokens in the message are that token
@@ -87,10 +88,10 @@ def calculate_sms_tf(vocabulary: list[str], sms: str) -> list[float] | None:
         vocabulary (list[str]): set of all words present in the samples
         sms (str): the given SMS message
     Returns:
-        list[float]: term frequency calculations given this message for each word in our vocabulary
+        ndarray[np.float64]: term frequency calculations given this message for each word in our vocabulary
         Will return None if the given SMS has no tokens. For example, with certain cleaning methods an SMS will have no tokens
     """
-    term_frequency_list = []
+    term_frequency_list = np.empty(len(vocabulary), dtype=float)
     tokens = tokenize_sms(sms)
     token_length = len(tokens)
     if token_length == 0:
@@ -98,46 +99,37 @@ def calculate_sms_tf(vocabulary: list[str], sms: str) -> list[float] | None:
     token_counts = dict()
     for token in tokens:
         token_counts[token] = token_counts.get(token, 0) + 1
-    for token in vocabulary:
-        term_frequency_list.append(token_counts.get(token, 0) / token_length)
+    for i, token in enumerate(vocabulary):
+        term_frequency_list[i] = (token_counts.get(token, 0) / token_length)
     return term_frequency_list
 
 
-def calculate_sms_idf(vocabulary: list[str], sms: str, tokenized_sms_list: list[list[str]], testing_sample=False) -> list[float] | None:
+def calculate_idf(vocabulary: list[str], tokenized_sms_list: list[list[str]]) -> npt.NDArray[np.float64]:
     """
-    Calculate the inverse document frequency for a given SMS message
+    Calculate the inverse document frequency for a given set of SMS messages
     For each token in the vocabulary: log( (total # of SMS messages) / (# of messages this word appears in) ) for each word in this SMS
     If a word is in the vocabulary but not in this SMS, it gets a 0
 
     Args:
         vocabulary (list[str]): set of all words present in the samples
-        sms (str): the given SMS message
         sms_list (list[str]): list of all SMS messages
-        testing_sample (bool=False): if this sample is not in the training set, must add one to the denominator
     Returns:
         list[float]: inverse document frequency calculations given this message for each word in our vocabulary and this set of SMS messages
         Will return None if the given SMS has no tokens. For example, with certain cleaning methods an SMS will have no tokens
     """
-    inverse_document_frequency_list = []
-    tokenized_sms = tokenize_sms(sms)
-    token_length = len(tokenized_sms)
-    if token_length == 0:
-        return None
+    inverse_document_frequency_list = np.empty(len(vocabulary), dtype=float)
     document_total = len(tokenized_sms_list)
     # Calculate idf for this sms
-    for token in vocabulary:
-        if token in tokenized_sms:
-            token_appearance_count = 1 if testing_sample else 0
-            for tokens in tokenized_sms_list:
-                if token in tokens:
-                    token_appearance_count += 1
-            inverse_document_frequency_list.append(math.log10(document_total / token_appearance_count))
-        else:
-            inverse_document_frequency_list.append(0)
+    for i, token in enumerate(vocabulary):
+        token_appearance_count = 0
+        for tokenized_sms in tokenized_sms_list:
+            if token in tokenized_sms:
+                token_appearance_count += 1
+        inverse_document_frequency_list[i] = math.log10(document_total / token_appearance_count)
     return inverse_document_frequency_list
 
 
-def calculate_n_dimensional_distance(p1: list[float], p2: list[float]) -> float:
+def calculate_n_dimensional_distance(p1: npt.NDArray[np.float64], p2: npt.NDArray[np.float64]) -> float:
     """
     Calculates the distance between two points in n-dimensional space
     Uses numpy in order to be more efficient
@@ -150,9 +142,7 @@ def calculate_n_dimensional_distance(p1: list[float], p2: list[float]) -> float:
     """
     if len(p1) != len(p2):
         raise ValueError('The two provided points do not have the same dimensionality')
-    n1 = np.array(p1)
-    n2 = np.array(p2)
-    result = np.sqrt(np.sum((n1 - n2)**2))
+    result = np.sqrt(np.sum((p1 - p2)**2))
     return result
 
 
@@ -162,6 +152,11 @@ def main():
         ham_list, spam_list = load_data(DATA_FILENAME)
     except FileNotFoundError as e:
         print(e)
+        exit()
+
+    # Make sure K is odd
+    if K % 2 == 0:
+        print('K must be an odd number!')
         exit()
 
     # shuffle so training/testing split is random each time
@@ -185,30 +180,26 @@ def main():
 
     # Convert to list so ordering stays consistent
     # The order of the vocabulary is the order the tokens must show up in the frequency dictionaries
-    vocabulary = list(set(ham_tokens + spam_tokens))
+    vocabulary = sorted(list(set(ham_tokens + spam_tokens)))
 
     # Pre-calculate this for the idf function
     tokenized_sms_list = []
     for sms in list(sms_samples.keys()):
         tokenized_sms_list.append(tokenize_sms(sms))
 
-    # Calculate term frequencies and inverse document frequencies for each SMS in the training set
-    # Also calculate the TF-IDF by multiplying both values together
+    # Calculate idf vector for this training set
+    inverse_document_frequencies = calculate_idf(vocabulary, tokenized_sms_list)
+
+    # Calculate term frequencies for each SMS in the training set
+    # Also calculate the TF-IDF by multiplying the TF by the global IDF we just calculated
     sms_term_frequencies = {}
-    sms_inverse_document_frequencies = {}
     sms_tf_idf = {}
     for sms in list(sms_samples.keys()):
         sms_tf = calculate_sms_tf(vocabulary, sms)
-        if sms_tf is None:
+        if sms_tf is None:  # is this okay? just skipping sms that are blank after cleaning and tokenizing
             continue
         sms_term_frequencies[sms] = sms_tf
-
-        sms_idf = calculate_sms_idf(vocabulary, sms, tokenized_sms_list)
-        if sms_idf is None:
-            continue
-        sms_inverse_document_frequencies[sms] = sms_idf
-
-        sms_tf_idf[sms] = [tf * idf for tf, idf in zip(sms_tf, sms_idf)]
+        sms_tf_idf[sms] = sms_tf * inverse_document_frequencies
     
     # Testing
     ground_truth = {}
@@ -220,29 +211,28 @@ def main():
     results = {}
     for sms in list(ground_truth.keys()):
         tf = calculate_sms_tf(vocabulary, sms)
-        idf = calculate_sms_idf(vocabulary, sms, tokenized_sms_list, True)
-        tf_idf = list(tf_val * idf_val for tf_val, idf_val in zip(tf, idf))
+        if tf is None:  # is this okay? just skipping sms that are blank after cleaning and tokenizing
+            continue
+    
+        tf_idf = tf * inverse_document_frequencies
 
         # Calculate the distance between this vector and the vector for every point in the training set
+        # NOTE: This could be made much more efficient by converting sms_tf_idf to a 2D numpy array and keeping a parallel array with the original SMS messages
+        # This would allow some highly efficient matrix multiplication to calculate all the distances at once instead of doing it in a loop like this
         distances = {}
-        print('doing calc')
         for training_sms, training_tf_idf in sms_tf_idf.items():
             distances[training_sms] = calculate_n_dimensional_distance(training_tf_idf, tf_idf)
-        print('finish calc')
         # Sort dictionary by value and select the K lowest values
         # If spam/ham has the majority in the K lowest values, then that is our prediction
         distances = dict(sorted(distances.items(), key=lambda item: item[1]))
         ham_count = 0
         spam_count = 0
-        for sms in list(distances.keys())[:K]:
-            if sms_samples[sms] == 0:
+        for distance_sms in list(distances.keys())[:K]:
+            if sms_samples[distance_sms] == 0:
                 ham_count += 1
             else:
                 spam_count += 1
         results[sms] = 0 if ham_count > spam_count else 1
-        print(f'{sms[:150]}...')
-        print('ham' if ham_count > spam_count else 'spam')
-        print()
         
     # Results and performance metrics
     tp = 0
